9bfc5c75a7422d6683828b2b76a5a3cd4aca3809
Aponia <brianmv@uci.edu>
2023-11-26 17:19:49 -0800

    feat: ✨ export session from the entry of core

---
+++ .changeset/strange-gorillas-occur.md
@@ -0,0 +1,7 @@
+---
+'@aponia.js/core': patch
+'@aponia.js/adapter-prisma': patch
+'@aponia.js/sveltekit': patch
+---
+
+feat: export the session from the index of core
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
-export * from './controllers/middleware-auth'
 export * from './controllers/client-auth'
+export * from './controllers/middleware-auth'
+export * from './controllers/session'
 export * from './types'
