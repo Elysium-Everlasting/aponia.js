e8a71f97c0329bb2fe888e1e881c021d84778819
Aponia <brianmv@uci.edu>
2023-12-02 07:42:17 -0800

    fix(sveltekit-prisma): 🐛 property in hooks.server.ts

--- examples/sveltekit-prisma/src/hooks.server.ts
+++ examples/sveltekit-prisma/src/hooks.server.ts
@@ -11,9 +11,7 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private'
 const authHandle = SvelteKitAuth({
   adapter: adapterPrisma(prisma),
   session: {
-    pages: {
-      logoutRedirect: '/',
-    },
+    logoutRedirect: '/',
   },
   providers: [
     GitHub({
