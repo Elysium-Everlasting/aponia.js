ee5b140de9fac5da958b96c446eff182a0318f32
Aponia <brianmv@uci.edu>
2023-12-02 07:42:10 -0800

    test: 🚨 nonce

--- packages/core/tests/security/checks.test.ts
+++ packages/core/tests/security/checks.test.ts
@@ -3,7 +3,7 @@ import crypto from 'node:crypto'
 import * as oauth from 'oauth4webapi'
 import { beforeAll, describe, test, expect } from 'vitest'
 
-import { pkce, state } from '../../src/security/checks'
+import { pkce, state, nonce } from '../../src/security/checks'
 import { DEFAULT_COOKIES_OPTIONS } from '../../src/security/cookie'
 import { DEFAULT_JWT_OPTIONS } from '../../src/security/jwt'
 import type { InternalRequest } from '../../src/types'
@@ -195,6 +195,82 @@ describe('checks', () => {
         })
 
         expect(stateValue).toEqual(value)
+      })
+    })
+  })
+
+  describe('nonce', () => {
+    describe('create', () => {
+      test('correctly creates value and cookie with default settings', async () => {
+        const [value, cookie] = await nonce.create({
+          jwt: DEFAULT_JWT_OPTIONS,
+          cookies: DEFAULT_COOKIES_OPTIONS,
+        })
+
+        expect(cookie.options).toEqual(DEFAULT_COOKIES_OPTIONS.pkceCodeVerifier.options)
+        expect(value.length).toEqual(43)
+      })
+    })
+
+    describe('use', () => {
+      test('returns expectNoNonce if not using nonce check', async () => {
+        const [value, cookie] = await nonce.use(exampleInternalRequest, {
+          checks: [],
+          cookies: DEFAULT_COOKIES_OPTIONS,
+          jwt: DEFAULT_JWT_OPTIONS,
+        })
+
+        expect(value).toEqual(oauth.expectNoNonce)
+        expect(cookie).toEqual(null)
+      })
+
+      test('throws error if nonce cookie is missing', async () => {
+        await expect(
+          nonce.use(exampleInternalRequest, {
+            checks: ['nonce'] as any,
+            cookies: DEFAULT_COOKIES_OPTIONS,
+            jwt: DEFAULT_JWT_OPTIONS,
+          }),
+        ).rejects.toThrow()
+      })
+
+      test('throws error if nonce value could not be parsed', async () => {
+        const internalRequest: InternalRequest = {
+          ...exampleInternalRequest,
+          cookies: {
+            [DEFAULT_COOKIES_OPTIONS.nonce.name]: 'invalid',
+          },
+        }
+
+        await expect(
+          nonce.use(internalRequest, {
+            checks: ['nonce'] as any,
+            cookies: DEFAULT_COOKIES_OPTIONS,
+            jwt: DEFAULT_JWT_OPTIONS,
+          }),
+        ).rejects.toThrow()
+      })
+
+      test('returns original value and cookie if valid', async () => {
+        const [value, cookie] = await nonce.create({
+          jwt: DEFAULT_JWT_OPTIONS,
+          cookies: DEFAULT_COOKIES_OPTIONS,
+        })
+
+        const internalRequest: InternalRequest = {
+          ...exampleInternalRequest,
+          cookies: {
+            [cookie.name]: cookie.value,
+          },
+        }
+
+        const [nonceValue] = await nonce.use(internalRequest, {
+          checks: ['nonce'] as any,
+          cookies: DEFAULT_COOKIES_OPTIONS,
+          jwt: DEFAULT_JWT_OPTIONS,
+        })
+
+        expect(nonceValue).toEqual(value)
       })
     })
   })
