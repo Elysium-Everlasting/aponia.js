8b041890c85785f698e6b6c56c4ad0063cf8f333
Aponia <brianmv@uci.edu>
2023-12-02 14:50:22 -0800

    fix: 🐛 typescript error

--- packages/core/src/aponia-auth.d.ts
+++ packages/core/src/aponia-auth.d.ts
@@ -1,6 +1,7 @@
 declare global {
   declare module '@auth/core/providers/oauth' {
     type OAuth2TokenEndpointResponse = import('oauth4webapi').OAuth2TokenEndpointResponse
+    type OpenIDTokenEndpointResponse = import('oauth4webapi').OpenIDTokenEndpointResponse
     type Awaitable = import('./utils/types').Awaitable
     type InternalRequest = import('./types').InternalRequest
     type Nullish = import('./utils/types').Nullish
@@ -8,7 +9,7 @@ declare global {
     interface OAuth2Config<Profile> {
       onAuth?: (
         user: Profile,
-        tokens: OAuth2TokenEndpointResponse,
+        tokens: OAuth2TokenEndpointResponse | OpenIDTokenEndpointResponse,
       ) => Awaitable<InternalRequest | Nullish>
     }
   }
