9c3df3ecdd705fec8d7612e5777a5dcba9d4349c
Aponia <brianmv@uci.edu>
2023-12-05 09:08:21 -0800

    feat: ✨ auth middleware class

---
+++ packages/core/src/auth.ts
@@ -0,0 +1,83 @@
+import {
+  DEFAULT_FORGOT_ROUTE,
+  DEFAULT_LOGOUT_ROUTE,
+  DEFAULT_RESET_ROUTE,
+  DEFAULT_UPDATE_ROUTE,
+} from './constants'
+
+export interface AuthConfig {
+  pages?: Partial<AuthPages>
+  callbacks?: Partial<AuthCallbacks>
+}
+
+export interface AuthPages {
+  logout: PageEndpoint
+  update: PageEndpoint
+  forgot: PageEndpoint
+  reset: PageEndpoint
+  logoutRedirect?: string
+}
+
+export type AuthCallbacks = {
+  [k in keyof AuthPages]?: (request: Aponia.Request) => Promise<Aponia.Response>
+}
+
+export interface PageEndpoint {
+  route: string
+  methods: string[]
+  redirect?: string
+}
+
+export class Auth {
+  callbacks?: Partial<AuthCallbacks>
+
+  pages: AuthPages
+
+  constructor(config: AuthConfig) {
+    this.pages = {
+      logout: config.pages?.logout ?? { route: DEFAULT_LOGOUT_ROUTE, methods: ['POST'] },
+      update: config.pages?.update ?? { route: DEFAULT_UPDATE_ROUTE, methods: ['POST'] },
+      forgot: config.pages?.forgot ?? { route: DEFAULT_FORGOT_ROUTE, methods: ['POST'] },
+      reset: config.pages?.reset ?? { route: DEFAULT_RESET_ROUTE, methods: ['POST'] },
+    }
+
+    this.callbacks = config.callbacks
+  }
+
+  public async handle(request: Aponia.Request): Promise<Aponia.Response> {
+    if (this.matches(request, this.pages.logout)) {
+      request.action = 'logout'
+      return (await this.callbacks?.logout?.(request)) ?? {} // this.session.invalidateSession(request)
+    }
+
+    if (this.matches(request, this.pages.update)) {
+      request.action = 'update'
+      return (await this.callbacks?.update?.(request)) ?? {}
+    }
+
+    if (this.matches(request, this.pages.forgot)) {
+      request.action = 'forgot'
+      return (await this.callbacks?.forgot?.(request)) ?? {}
+    }
+
+    if (this.matches(request, this.pages.reset)) {
+      request.action = 'reset'
+      return (await this.callbacks?.reset?.(request)) ?? {}
+    }
+
+    return {}
+  }
+
+  /**
+   * Whether a {@link Aponia.Request} matches a {@link PageEndpoint}.
+   */
+  private matches(request: Aponia.Request, pageEndpoint: PageEndpoint): boolean {
+    return (
+      pageEndpoint.route === request.url.pathname && pageEndpoint.methods.includes(request.method)
+    )
+  }
+}
+
+export function createAuth(config: AuthConfig): Auth {
+  return new Auth(config)
+}
--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,3 +1,16 @@
+//----------------------------------------------------------------------------------------
+// Pages.
+//----------------------------------------------------------------------------------------
+export const DEFAULT_LOGOUT_REDIRECT = '/auth/login'
+export const DEFAULT_LOGOUT_ROUTE = '/auth/logout'
+export const DEFAULT_UPDATE_ROUTE = '/auth/update'
+export const DEFAULT_FORGOT_ROUTE = '/auth/forgot'
+export const DEFAULT_RESET_ROUTE = '/auth/reset'
+export const DEFAULT_LOGIN_ROUTE = '/auth/login'
+export const DEFAULT_REGISTER_ROUTE = '/auth/register'
+export const DEFAULT_CALLBACK_ROUTE = '/auth/callback'
+export const DEFAULT_CALLBACK_REDIRECT = '/'
+
 export const ACTIONS = [
   'login',
   'register',
@@ -6,7 +19,6 @@ export const ACTIONS = [
   'update',
   'forgot',
   'reset',
-  'none',
 ] as const
 
 export type Action = (typeof ACTIONS)[number]
