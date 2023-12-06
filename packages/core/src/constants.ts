190a2f6a146f3ba5be3632074f6175eb1ef2f725
Aponia <brianmv@uci.edu>
2023-12-05 22:04:57 -0800

    feat: ✨ new cookie store setup thing

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -11,6 +11,15 @@ export const DEFAULT_REGISTER_ROUTE = '/auth/register'
 export const DEFAULT_CALLBACK_ROUTE = '/auth/callback'
 export const DEFAULT_CALLBACK_REDIRECT = '/'
 
+//----------------------------------------------------------------------------------------
+// Miscellaneous.
+//----------------------------------------------------------------------------------------
+export const FIFTEEN_MINUTES_IN_SECONDS = 60 * 15
+export const HOUR_IN_SECONDS = 4 * FIFTEEN_MINUTES_IN_SECONDS
+export const DAY_IN_SECONDS = 24 * HOUR_IN_SECONDS
+export const WEEK_IN_SECONDS = 7 * DAY_IN_SECONDS
+export const IS_BROWSER = typeof window !== 'undefined'
+
 export const ACTIONS = [
   'login',
   'register',
@@ -20,7 +29,6 @@ export const ACTIONS = [
   'forgot',
   'reset',
 ] as const
-
 export type Action = (typeof ACTIONS)[number]
 
 //----------------------------------------------------------------------------------------
@@ -28,3 +36,27 @@ export type Action = (typeof ACTIONS)[number]
 //----------------------------------------------------------------------------------------
 export const ISSUER = 'aponia.js'
 export const KEY_INFO = 'Auth.js Generated Encryption Key'
+
+//----------------------------------------------------------------------------------------
+// Security.
+//----------------------------------------------------------------------------------------
+export const SALT = ''
+export const DEFAULT_SECRET = 'secret'
+export const DEFAULT_ACCESS_TOKEN_AGE = HOUR_IN_SECONDS
+export const DEFAULT_REFRESH_TOKEN_AGE = WEEK_IN_SECONDS
+export const DEFAULT_CHECKS = ['pkce']
+export const PKCE_MAX_AGE = FIFTEEN_MINUTES_IN_SECONDS
+export const STATE_MAX_AGE = FIFTEEN_MINUTES_IN_SECONDS
+export const NONCE_MAX_AGE = FIFTEEN_MINUTES_IN_SECONDS
+export const DEFAULT_COOKIE_NAME = 'aponia-auth'
+export const DEFAULT_SECURE_PREFIX = '__Secure-'
+export const DEFAULT_RESPONSE_TYPE = 'code'
+
+export const ACCESS_TOKEN_NAME = 'access-token'
+export const REFRESH_TOKEN_NAME = 'refresh-token'
+export const CALLBACK_URL_NAME = 'callback-url'
+export const CSRF_TOKEN_NAME = 'csrf-token'
+export const PKCE_NAME = 'pkce.code-verifier'
+export const STATE_NAME = 'state'
+export const NONCE_NAME = 'nonce'
+export const HOST_PREFIX = '__Host-'
