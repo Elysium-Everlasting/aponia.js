ce687951c20aa10b7ff1a6c6192a42916c9ea97b
Aponia <brianmv@uci.edu>
2023-12-06 17:08:51 -0800

    refactor: ♻️ controller

--- packages/core/src/controllers/session.ts
+++ packages/core/src/controllers/session.ts
@@ -1,15 +1,88 @@
-import type { PluginCoordinator } from '../plugin'
-import type { Cookie } from '../security/cookie'
+import { ACCESS_TOKEN_NAME, DEFAULT_ACCESS_TOKEN_AGE } from '../constants'
+import {
+  getCookiePrefix,
+  type Cookie,
+  type CookieOption,
+  type CreateCookiesOptions,
+} from '../security/cookie'
 import type { Awaitable } from '../utils/types'
 
-export abstract class SessionController {
-  abstract createSessionFromUser(user: Aponia.User): Awaitable<Aponia.Session | undefined>
+export interface SessionControllerConfig {
+  encode?: (session: Aponia.Session) => Awaitable<string>
+  decode?: (token: string) => Awaitable<Aponia.Session | undefined>
+  cookie?: CreateCookiesOptions
+}
 
-  abstract createCookiesFromSession(session: Aponia.Session): Awaitable<Cookie[]>
+export class SessionController {
+  config: SessionControllerConfig
 
-  abstract parseSessionFromCookies(
+  encode: (session: Aponia.Session) => Awaitable<string>
+
+  decode: (token: string) => Awaitable<Aponia.Session | undefined>
+
+  cookies: ClientCookiesOptions
+
+  constructor(config: SessionControllerConfig = {}) {
+    this.config = config
+
+    this.encode = config.encode ?? ((value) => JSON.stringify(value))
+
+    this.decode = config.decode ?? ((value) => JSON.parse(value))
+
+    this.cookies = createClientCookiesOptions({
+      ...config.cookie,
+      serialize: {
+        path: '/',
+        sameSite: 'lax',
+        maxAge: DEFAULT_ACCESS_TOKEN_AGE,
+        ...config.cookie?.serialize,
+      },
+    })
+  }
+
+  async createSessionFromUser(user: Aponia.User): Promise<Aponia.Session | undefined> {
+    return user
+  }
+
+  async createCookiesFromSession(session: Aponia.Session): Promise<Cookie[]> {
+    const accessToken = await this.encode(session)
+
+    const sessionCookie: Cookie = {
+      name: this.cookies.accessToken.name,
+      value: accessToken,
+      options: this.cookies.accessToken.options,
+    }
+
+    return [sessionCookie]
+  }
+
+  async parseSessionFromCookies(
     cookies: Record<string, string>,
-  ): Awaitable<Aponia.Session | undefined>
+  ): Promise<Aponia.Session | undefined> {
+    const rawAccessToken = cookies[this.cookies.accessToken.name]
 
-  abstract initialize?: (plugin: PluginCoordinator) => unknown
+    if (rawAccessToken == null) {
+      return
+    }
+
+    const accessToken = await this.decode(rawAccessToken)
+
+    return accessToken
+  }
+}
+
+export interface ClientCookiesOptions {
+  accessToken: CookieOption
+}
+
+export function createClientCookiesOptions(options?: CreateCookiesOptions): ClientCookiesOptions {
+  const cookiePrefix = getCookiePrefix(options)
+  const serializeOptions = { ...options?.serialize }
+
+  return {
+    accessToken: {
+      name: `${cookiePrefix}.${ACCESS_TOKEN_NAME}`,
+      options: serializeOptions,
+    },
+  }
 }
