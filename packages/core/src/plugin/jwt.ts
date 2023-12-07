030d113a8d390677db5d65f80ec2b53142040b88
Aponia <brianmv@uci.edu>
2023-12-06 16:17:59 -0800

    feat: ✨ options for toggling

--- packages/core/src/plugin/jwt.ts
+++ packages/core/src/plugin/jwt.ts
@@ -5,24 +5,31 @@ import type { Plugin } from '.'
 
 export interface JwtPluginOptions {
   secret?: string
+  jwt?: boolean
+  checker?: boolean
 }
 
 export function jwtPlugin(options: JwtPluginOptions = {}): Plugin {
   const secret = options.secret ?? DEFAULT_SECRET
 
-  const checkerEncode = async (value: string): Promise<string> => {
-    return await encode({ secret, token: { value } })
-  }
-
-  const checkerDecode = async (token: string): Promise<string> => {
-    return await decode({ secret, token }).then((result) => result?.['value'])
-  }
-
   return {
     name: 'jwt',
     setup: (plugin) => {
-      plugin.emit('jwt', { secret, encode, decode })
-      plugin.emit('checker', { encode: checkerEncode, decode: checkerDecode })
+      if (options.jwt !== false) {
+        plugin.emit('jwt', { secret, encode, decode })
+      }
+
+      if (options.checker !== false) {
+        const checkerEncode = async (value: string): Promise<string> => {
+          return await encode({ secret, token: { value } })
+        }
+
+        const checkerDecode = async (token: string): Promise<string> => {
+          return await decode({ secret, token }).then((result) => result?.['value'])
+        }
+
+        plugin.emit('checker', { encode: checkerEncode, decode: checkerDecode })
+      }
     },
   }
 }
