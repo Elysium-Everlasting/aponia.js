b277c1e9ec2c49dd3de2c3d5891cf41fbab702eb
Aponia <brianmv@uci.edu>
2024-01-01 10:14:12 -0800

    feat: ✨ google oidc example with core api

--- packages/core/src/utils/types.ts
+++ packages/core/src/utils/types.ts
@@ -1,3 +1,15 @@
 export type Awaitable<T> = T | PromiseLike<T>
 
 export type Nullish = null | undefined | void
+
+export type IsAny<T> = 0 extends 1 & T ? true : false
+
+export type IsFunction<T> = T extends (...args: any[]) => any ? true : false
+
+export type DeepPartial<T> = IsAny<T> extends true
+  ? any
+  : IsFunction<NonNullable<T>> extends true
+  ? T
+  : T extends Record<PropertyKey, any>
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : T
