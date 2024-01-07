7466ce8c7c5441abce68fd90ef1045ebba530b2c
Aponia <brianmv@uci.edu>
2024-01-07 06:44:16 -0800

    feat(core): ✨ jwt session controller converted to plugin

---
+++ packages/core/src/plugins/session/jwt.ts
@@ -0,0 +1,29 @@
+import { DEFAULT_ACCESS_TOKEN_AGE, DEFAULT_SECRET } from '../../constants'
+import { encode, decode } from '../../security/jwt'
+
+import { SessionPlugin, type SessionPluginConfig } from './index'
+
+export interface JwtSessionPluginConfig extends SessionPluginConfig {
+  secret?: string
+}
+
+export class JwtSessionPlugin extends SessionPlugin {
+  secret: string
+
+  constructor(config: JwtSessionPluginConfig = {}) {
+    const maxAge = config.cookie?.serialize?.maxAge ?? DEFAULT_ACCESS_TOKEN_AGE
+    const secret = config.secret ?? DEFAULT_SECRET
+
+    config.cookie ??= {}
+    config.cookie.serialize ??= {}
+    config.cookie.serialize.maxAge = maxAge
+
+    config.secret = secret
+    config.encode ??= (session) => encode({ token: session, secret, maxAge })
+    config.decode ??= (token) => decode({ token, secret })
+
+    super(config)
+
+    this.secret = config.secret
+  }
+}
