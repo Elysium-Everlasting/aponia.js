0db451ffc8e216ebd2a503cb0cc35a53f534f49a
Aponia <brianmv@uci.edu>
2024-01-09 16:45:03 -0800

    feat(prisma): ✨ guard for account and session management

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -1,3 +1,4 @@
+import '@aponia.js/core/types'
 import type { Plugin, PluginContext, PluginOptions } from '@aponia.js/core/plugins/plugin'
 
 export class PrismaSessionPlugin implements Plugin {
@@ -6,5 +7,18 @@ export class PrismaSessionPlugin implements Plugin {
   initialize(context: PluginContext, options: PluginOptions): void {
     context
     options
+    context.router.postHandle(this.handle.bind(this))
+  }
+
+  async handle(_request: Aponia.Request, response?: Aponia.Response): Promise<void> {
+    // Only perform account and session management if all of these properties are defined.
+    if (
+      response?.user == null ||
+      response.providerId == null ||
+      response.providerType == null ||
+      response.providerAccountId == null
+    ) {
+      return
+    }
   }
 }
