3c6923ab4143f720f2789ea01fc0e83762e5243e
Aponia <brianmv@uci.edu>
2024-01-09 16:41:15 -0800

    feat(prisma): ✨ mvp entry file

---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,10 @@
+import type { Plugin, PluginContext, PluginOptions } from '@aponia.js/core/plugins/plugin'
+
+export class PrismaSessionPlugin implements Plugin {
+  constructor() {}
+
+  initialize(context: PluginContext, options: PluginOptions): void {
+    context
+    options
+  }
+}
