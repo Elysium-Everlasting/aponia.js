c02e6a519189c8ec204d72dd2fdf1c8e68a23466
Aponia <brianmv@uci.edu>
2024-03-30 12:39:26 -0700

    chore: 🔧 remove .svelte-kit temp directory

--- examples/auth.js/sveltekit/.svelte-kit/types/src/routes/proxy+layout.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { LayoutServerLoad } from './$types'
-
-import { jwtSession } from '$lib/server/auth'
-
-export const load = async (event: Parameters<LayoutServerLoad>[0]) => {
-  const session = await jwtSession.parseSessionFromCookies(event.cookies)
-  return { session }
-}
