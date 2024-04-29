bef47de6331cfba00ac099be786fff5c838f2beb
Aponia <brianmv@uci.edu>
2024-04-29 07:37:28 -0700

    feat: ✨ sveltekit utility export

---
+++ .changeset/strange-pens-learn.md
@@ -0,0 +1,7 @@
+---
+'@aponia.js/sveltekit': patch
+'@aponia.js/auth.js': patch
+'@aponia.js/core': patch
+---
+
+feat: sveltekit exports utility transform function
--- packages/sveltekit/src/index.ts
+++ packages/sveltekit/src/index.ts
@@ -1,17 +1,22 @@
 import { Auth, type AuthConfig } from '@aponia.js/core/auth'
-import type { Handle } from '@sveltejs/kit'
+import type { Handle, RequestEvent } from '@sveltejs/kit'
+
+export function eventToRequest(event: RequestEvent): Aponia.Request {
+  const request: Aponia.RequestInput = {
+    url: event.url,
+    method: event.request.method,
+    headers: event.request.headers,
+    cookies: event.cookies,
+    event,
+  }
+  return request
+}
 
 export function sveltekit(authOrConfig: Auth | AuthConfig): Handle {
   const auth = 'router' in authOrConfig ? authOrConfig : new Auth(authOrConfig)
 
   const handle: Handle = async ({ event, resolve }) => {
-    const request: Aponia.RequestInput = {
-      url: event.url,
-      method: event.request.method,
-      headers: event.request.headers,
-      cookies: event.cookies,
-      event,
-    }
+    const request = eventToRequest(event)
 
     const authResponse = await auth.handle(request)
 
